85648d04311c0853a7bd4496dbde03eb3a84d159
Sahil-Bishnoi107 <sahilbishnoi107@gmail.com>
2026-06-08 11:36:03 +0530

    testsListpage without testing

---
+++ lib/TestsPage/Domain/entities/Test.dart
@@ -0,0 +1,26 @@
+import 'package:study_mate/Home/Domain/Entities/Question.dart';
+
+class Test{
+  String id;
+  String name;
+  int totalQuestions;
+  String status;
+  String subject;
+  int time;
+  String diffiucluty;
+  Test({required this.id,required this.name,required this.totalQuestions,required this.subject,required this.status,required this.time,required this.diffiucluty});
+  
+    factory Test.fromJson(Map<String,dynamic> mp){
+  
+    String id = mp['id'] ?? "No id found";
+    String name = mp['name'] ?? "No name found";
+    int totalQuestions = mp['total_questions']  ?? 0;
+    
+    String status = mp['status'] ?? "Not Known";
+    
+    String subject = mp['subject'] ?? "Unknown";
+    int time = mp['time'] ?? 0;
+    String diffiucluty = mp['difficulty'] ?? "easy";
+    return Test(id: id, name: name, totalQuestions: totalQuestions, subject: subject, status: status, time: time,diffiucluty: diffiucluty);
+  }
+}
